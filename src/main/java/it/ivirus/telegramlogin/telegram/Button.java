a894ca384d16037a0c8ac378eb7766c695d58245
iVirus <edoardomannino@gmail.com>
2021-10-07 00:31:55 +0200

    removed

--- src/main/java/it/ivirus/telegramlogin/telegram/Button.java
+++
@@ -1,8 +0,0 @@
-package it.ivirus.telegramlogin.telegram;
-
-public abstract class Button {
-
-    public abstract void onClickButton(String sender, String command, String[] args);
-
-    public abstract void onClickButton(String sender, String command);
-}
