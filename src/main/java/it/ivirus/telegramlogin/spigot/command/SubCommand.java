ee5d6ae2759b8b2ea61b004a439a1f41f8dcd375
zHeroandre <75711194+zHeroandre@users.noreply.github.com>
2021-10-11 11:41:39 +0200

    add abstract class SubCommand

---
+++ src/main/java/it/ivirus/telegramlogin/spigot/command/SubCommand.java
@@ -0,0 +1,10 @@
+package it.ivirus.telegramlogin.spigot.command;
+
+import it.ivirus.telegramlogin.TelegramLogin;
+import org.bukkit.command.CommandSender;
+
+public abstract class SubCommand {
+    protected final TelegramLogin plugin = TelegramLogin.getInstance();
+
+    public abstract boolean onCommand(CommandSender sender, String[] args);
+}
