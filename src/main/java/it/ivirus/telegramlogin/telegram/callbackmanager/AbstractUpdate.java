583704d10570e84704c5b05403d95d4a43ad3ff3
BuildTools <unconfigured@null.spigotmc.org>
2021-10-14 15:13:55 +0200

    renames

--- src/main/java/it/ivirus/telegramlogin/telegram/callbackmanager/AbstractUpdate.java
+++ src/main/java/it/ivirus/telegramlogin/telegram/callbackmanager/AbstractUpdate.java
@@ -5,7 +5,7 @@ import it.ivirus.telegramlogin.data.PlayerData;
 import it.ivirus.telegramlogin.telegram.TelegramBot;
 import org.telegram.telegrambots.meta.api.objects.Update;
 
-public abstract class CallbackCommand {
+public abstract class AbstractUpdate {
     protected final PlayerData playerData = PlayerData.getInstance();
     protected final TelegramLogin plugin = TelegramLogin.getInstance();
 
