85b7ef09de08a4f3b7d250b8ff03b5c00e046027
iVirus <edoardomannino@gmail.com>
2021-11-05 12:04:05 +0100

    added velocity support

---
+++ src/main/java/it/ivirus/telegramlogin/velocity/TelegramLoginVelocity.java
@@ -0,0 +1,31 @@
+package it.ivirus.telegramlogin.velocity;
+
+import com.google.inject.Inject;
+import com.velocitypowered.api.event.Subscribe;
+import com.velocitypowered.api.event.proxy.ProxyInitializeEvent;
+import com.velocitypowered.api.plugin.Plugin;
+import com.velocitypowered.api.proxy.ProxyServer;
+import com.velocitypowered.api.proxy.messages.MinecraftChannelIdentifier;
+import it.ivirus.telegramlogin.velocity.listener.MessageListener;
+import it.ivirus.telegramlogin.velocity.listener.PlayerListener;
+import lombok.Getter;
+
+@Plugin(id = "telegramlogin", name = "TelegramLogin", version = "1.0.1",
+        url = "https://discord.io/hoxija", authors = {"iVirus"})
+public class TelegramLoginVelocity {
+    @Getter
+    private final ProxyServer server;
+
+    @Inject
+    public TelegramLoginVelocity(ProxyServer server) {
+        this.server = server;
+    }
+
+    @Subscribe
+    public void onProxyInitialization(ProxyInitializeEvent event) {
+        server.getEventManager().register(this, new PlayerListener());
+        server.getEventManager().register(this, new MessageListener());
+        server.getChannelRegistrar().register(MinecraftChannelIdentifier.create("hxj", "telegramlogin"));
+    }
+
+}
---
+++ src/main/java/it/ivirus/telegramlogin/velocity/listener/MessageListener.java
@@ -0,0 +1,43 @@
+package it.ivirus.telegramlogin.velocity.listener;
+
+import com.google.common.io.ByteArrayDataInput;
+import com.google.common.io.ByteStreams;
+import com.velocitypowered.api.event.Subscribe;
+import com.velocitypowered.api.event.connection.PluginMessageEvent;
+import com.velocitypowered.api.proxy.ServerConnection;
+import it.ivirus.telegramlogin.data.PlayerData;
+
+import java.util.UUID;
+
+public class MessageListener {
+
+    @SuppressWarnings("UnstableApiUsage")
+    @Subscribe
+    public void onMessageReceived(PluginMessageEvent event) {
+
+        if (!(event.getIdentifier().getId().equalsIgnoreCase("hxj:telegramlogin"))
+                || !(event.getSource() instanceof ServerConnection))
+            return;
+
+        ByteArrayDataInput input = ByteStreams.newDataInput(event.getData());
+
+        String subChannel = input.readUTF();
+        if (!subChannel.equals("cache")) return;
+
+        String action = input.readUTF();
+        UUID playerUUID = UUID.fromString(input.readUTF());
+        switch (action.toLowerCase()) {
+            case "add": {
+                PlayerData.getInstance().getBungeePendingPlayers().add(playerUUID);
+                break;
+            }
+            case "remove": {
+                PlayerData.getInstance().getBungeePendingPlayers().remove(playerUUID);
+                break;
+            }
+            default:
+                return;
+        }
+    }
+
+}
